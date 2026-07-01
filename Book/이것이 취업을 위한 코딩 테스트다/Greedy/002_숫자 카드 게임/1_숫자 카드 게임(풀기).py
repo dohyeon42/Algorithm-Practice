1e545fa232c5888e8e21dfccd264319d015c8240
Dohyeon Yun <ehgus140316@naver.com>
2026-07-01 20:17:35 +0900

    약간의 수정
    붙여넣기 오류로 인한 수정

--- Book/이것이 취업을 위한 코딩 테스트다/Greedy/002_숫자 카드 게임/1_숫자 카드 게임(풀기).py
+++ Book/이것이 취업을 위한 코딩 테스트다/Greedy/002_숫자 카드 게임/1_숫자 카드 게임(풀기).py
@@ -2,11 +2,11 @@ import time
 
 n, m = map(int, input().split())
 
+start_time = time.time()
 # for i in range(n):
 #     result.append(list(map(int, input().split())))
 result = [list(map(int, input().split())) for i in range(n)]
 
-start_time = time.time()
 result = max([min(result[i]) for i in range(n)])
 
 print(result)
